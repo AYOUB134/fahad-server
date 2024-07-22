c67a1da9f7194ec9c79b144f65dc5e5cd8b92fc8
AYOUB134 <113133550+AYOUB134@users.noreply.github.com>
2024-07-22 15:53:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.use(cors());
 app.use('/api/customers', customerRoutes);
 app.use('/api/expenses', expenseRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
